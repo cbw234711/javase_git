eeba6e74d932f00a52171bfc4e098cce5a9cfcde
cbw234711 <cbw234711@gmail.com>
2021-10-29 21:02:05 +0800

    javase demo1 for git test

---
+++ src/com/cbw/demo/Demo1.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2021, XXX有限公司
+ * FileName: Demo1
+ * Author:   Brandon
+ * Date:     2021/10/29 20:59
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.cbw.demo;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Brandon
+ * @create 2021/10/29
+ * @since 1.0.0
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
