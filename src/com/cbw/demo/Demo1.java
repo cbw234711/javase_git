c4c1f1c3328116421d3a3df1a29ff5ef8c121020
cbw234711 <cbw234711@gmail.com>
2021-11-03 15:05:23 +0800

    demo1 for git test1

--- src/com/cbw/demo/Demo1.java
+++ src/com/cbw/demo/Demo1.java
@@ -21,5 +21,6 @@ package com.cbw.demo;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("123");
     }
 }
